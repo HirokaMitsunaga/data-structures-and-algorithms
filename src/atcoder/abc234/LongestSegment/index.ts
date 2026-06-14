7fb7b42c79034b88609ccc092c0bd76df2204338
hiroka <mitsunagahrk87@gmail.com>
2026-06-14 17:50:47 +0900

    234Bのコードの修正

--- src/atcoder/abc234/LongestSegment/index.ts
+++ src/atcoder/abc234/LongestSegment/index.ts
@@ -1,41 +1,26 @@
 import * as fs from "fs";
 
+const input = fs.readFileSync(0, "utf8").trim().split("\n");
+
 const main = () => {
-  // N の取得
-  const input = fs.readFileSync(0, "utf8").trim().split("\n");
-  const N = Number(input[0]);
+  const values = input.slice(1).map((value) => {
+    const [x, y] = value.split(" ").map(Number);
+    return [x, y];
+  });
 
-  // 座標の格納用
-  const x: number[] = [];
-  const y: number[] = [];
-
-  // i=0~(N-1)
-  for (let i = 0; i < N; i++) {
-    // 入力の受け取り
-    const [xi, yi] = input[i + 1].split(" ").map(Number);
-
-    // 座標の格納
-    x.push(xi);
-    y.push(yi);
-  }
-
-  // 答え
-  let ans = 0;
-
-  // i=0~(N-1)
-  for (let i = 0; i < N; i++) {
-    // j=(i+1)~(N-1)
-    for (let j = i + 1; j < N; j++) {
-      // 距離の計算
-      const length = Math.sqrt((x[i] - x[j]) ** 2 + (y[i] - y[j]) ** 2);
-
-      // そこまでの値より大きければ更新
-      ans = Math.max(ans, length);
+  let max = 0;
+  for (let i = 0; i < values.length; i++) {
+    for (let j = i + 1; j < values.length; j++) {
+      let currentValue = 0;
+      currentValue = Math.sqrt(
+        (values[i][0] - values[j][0]) ** 2 + (values[i][1] - values[j][1]) ** 2,
+      );
+      if (max <= currentValue) {
+        max = currentValue;
+      }
     }
   }
-
-  // 答えの出力
-  console.log(ans);
+  console.log(max);
 };
 
 main();
